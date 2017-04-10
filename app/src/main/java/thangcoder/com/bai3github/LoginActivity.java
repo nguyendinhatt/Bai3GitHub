a3817bd30bf64b679eca7034b8b46af5d6efc745
nguyendinhatt <nguyendinhatt@gmail.com>
2017-04-11 02:59:28 +0700

    update1

---
+++ app/src/main/java/thangcoder/com/bai3github/LoginActivity.java
@@ -0,0 +1,10 @@
+package thangcoder.com.bai3github;
+
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by Ryan on 011-11/04/17.
+ */
+
+public class LoginActivity extends AppCompatActivity {
+}
